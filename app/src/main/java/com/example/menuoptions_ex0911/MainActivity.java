0d6d9f109dd91e51fab52a27a2fe8a6e5ecab19a
tv0823 <tv0823@bs.amalnet.k12.il>
2024-12-04 13:34:57 +0200

    xml done

--- app/src/main/java/com/example/menuoptions_ex0911/MainActivity.java
+++ app/src/main/java/com/example/menuoptions_ex0911/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.menuoptions_ex0911;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -21,5 +23,26 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu (Menu menu) {
         getMenuInflater().inflate(R.menu.main, menu);
         return super.onCreateOptionsMenu (menu);
+    }
+
+    public boolean onOptionsItemSelected(@Nullable MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.plusMi) {
+            
+        } else if(id == R.id.minusMi) {
+            
+        } else if (id == R.id.multMi) {
+            
+        } else if (id == R.id.divideMi) {
+            
+        } else {
+            
+        }
+        return true;
+    }
+
+    public boolean inputChecks(String st) {
+        
     }
 }
