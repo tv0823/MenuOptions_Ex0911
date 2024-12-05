1662472e46d1819f84d672b6b371ccc15f2f3118
tv0823 <tv0823@bs.amalnet.k12.il>
2024-12-05 18:19:45 +0200

    added mathematical actions and the way to show them simple

--- app/src/main/java/com/example/menuoptions_ex0911/MainActivity.java
+++ app/src/main/java/com/example/menuoptions_ex0911/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.menuoptions_ex0911;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
@@ -12,12 +15,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText num1Et, num2Et;
+    TextView ansTv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        num1Et = findViewById(R.id.num1Et);
+        num2Et = findViewById(R.id.num2Et);
+        ansTv = findViewById(R.id.ansTv);
     }
+
+    public void bigNumSimplifier(double value){
+        String scientificNotation = String.format("%.4e", value);
+        String[] parts = scientificNotation.split("e");
+        double base = Double.parseDouble(parts[0]) / 10.0;
+        int exponent = Integer.parseInt(parts[1]) + 1;
+
+        ansTv.setText(String.format("%.4f * 10^%d", base, exponent));
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu (Menu menu) {
@@ -27,22 +46,46 @@ public class MainActivity extends AppCompatActivity {
 
     public boolean onOptionsItemSelected(@Nullable MenuItem item) {
         int id = item.getItemId();
+        double total = 0;
+        String num1St = num1Et.getText().toString();
+        String num2St = num2Et.getText().toString();
 
-        if(id == R.id.plusMi) {
-            
-        } else if(id == R.id.minusMi) {
-            
-        } else if (id == R.id.multMi) {
-            
-        } else if (id == R.id.divideMi) {
-            
+        if(id == R.id.clearMi){
+            ansTv.setText("");
+            num1Et.setText("");
+            num2Et.setText("");
+            Toast.makeText(this, "Cleared fields", Toast.LENGTH_SHORT).show();
+        } else if (num1St.equals("-") || num1St.equals("-.") || num1St.equals("+") || num1St.equals("+.") || num1St.equals(".") || num2St.equals("-") || num2St.equals("-.") || num2St.equals("+") || num2St.equals("+.") || num2St.equals(".")){
+            Toast.makeText(this, "Wrong input", Toast.LENGTH_SHORT).show();
+        } else if (num1St.isEmpty() || num2St.isEmpty()) {
+            Toast.makeText(this, "One or more field is empty", Toast.LENGTH_SHORT).show();
         } else {
-            
-        }
-        return true;
-    }
+            double num1 = Double.parseDouble(num1St);
+            double num2 = Double.parseDouble(num2St);
 
-    public boolean inputChecks(String st) {
-        
+            if(id == R.id.plusMi) {
+                total = num1 + num2;
+                Toast.makeText(this, "Added fields", Toast.LENGTH_SHORT).show();
+            } else if(id == R.id.minusMi) {
+                total = num1 - num2;
+                Toast.makeText(this, "Subtracted fields", Toast.LENGTH_SHORT).show();
+            } else if (id == R.id.multMi) {
+                total = num1 * num2;
+                Toast.makeText(this, "Multiplied fields", Toast.LENGTH_SHORT).show();
+            } else if (id == R.id.divideMi) {
+                total = num1 / num2;
+                Toast.makeText(this, "Divided fields", Toast.LENGTH_SHORT).show();
+            }
+
+            if ((total > 1000000) || (total < -1000000)) {
+                bigNumSimplifier(total);
+            } else if(total > -1 || total < 1) {
+                ansTv.setText(total + "");
+            } else{
+                String.format("%.2f", total);
+            }
+        }
+
+        return true;
     }
 }
